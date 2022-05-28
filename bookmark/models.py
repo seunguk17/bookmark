82a1dc2ee3267e6269257b5bf4e9b6ba0ddd5e9d
Bseunguk <wonserwall@gmail.com>
2022-05-28 18:55:59 +0900

    Bookmark service

---
+++ bookmark/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+
+class Bookmark(models.Model):
+    site_name = models.CharField(max_length=100)
+    url = models.URLField('Site URL')
+
+    def __str__(self):
+        return self.site_name
+
+    def get_absolute_url(self):
+        return reverse('detail', args=[str(self.id)])
